d9d204d6e360d07df46a5c4c745db49bc980ee5e
AnkushYadav <yadavankush1811@gmail.com>
2025-12-18 14:38:43 +0530

    projects image and fonts are set

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Home</title>
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+
+    <body>
+        <header>
+            <div>
+                <p>Need help? Call us 112233344455</p>
+                <p>Summer sale discount off 60% off! Shop Now</p>
+                <p>2-3 business days delivery & free returns</p>
+            </div>
+            <div>
+                <p>Shop <span>Lite</span></p>
+                <ul>
+                    <li>HOME</li>
+                    <li>ABOUT</li>
+                    <li>SHOP</li>
+                    <li>BLOGS</li>
+                    <li>PAGES</li>
+                    <li>CONTACT</li>
+                </ul>
+                <ul>
+                    <li><img src="./images/search.svg" alt="search"></li>
+                    <li><img src="./images/profile.svg" alt="Profile"></li>
+                    <li><img src="./images/favourite.svg" alt="like"></li>
+                    <li><img src="./images/cart.svg" alt="cart"></li>
+                </ul>
+            </div>
+    </header>
+
+
+        <main></main>
+        <footer>
+          
+        </footer>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
